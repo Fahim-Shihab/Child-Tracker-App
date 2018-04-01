d805e713af3210582960553644b3b17a6167b552
debsourav33 <debsourav33@gmail.com>
2018-04-01 21:33:35 +0600

    First Commit

---
+++ ChildTrackerforParent/app/src/main/java/com/appdev/debsourav/childtrackerforparent/Call.java
@@ -0,0 +1,51 @@
+package com.appdev.debsourav.childtrackerforparent;
+
+/**
+ * Created by Deb Sourav on 3/29/2018.
+ */
+
+public class Call {
+    private String phNumber, callType, callDate, callDuration;
+
+    public Call() {
+    }
+
+    public Call(String phNumber, String callType, String callDate, String callDuration) {
+        this.phNumber = phNumber;
+        this.callType = callType;
+        this.callDate = callDate;
+        this.callDuration = callDuration;
+    }
+
+    public String getPhNumber() {
+        return phNumber;
+    }
+
+    public void setPhNumber(String phNumber) {
+        this.phNumber = phNumber;
+    }
+
+    public String getCallType() {
+        return callType;
+    }
+
+    public void setCallType(String callType) {
+        this.callType = callType;
+    }
+
+    public String getCallDate() {
+        return callDate;
+    }
+
+    public void setCallDate(String callDate) {
+        this.callDate = callDate;
+    }
+
+    public String getCallDuration() {
+        return callDuration;
+    }
+
+    public void setCallDuration(String callDuration) {
+        this.callDuration = callDuration;
+    }
+}
---
+++ ChildTrackerforParent/app/src/main/java/com/appdev/debsourav/childtrackerforparent/MainActivity.java
@@ -0,0 +1,46 @@
+package com.appdev.debsourav.childtrackerforparent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mRef;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRef= FirebaseDatabase.getInstance().getReference();
+        recyclerView= findViewById(R.id.rView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        FirebaseRecyclerAdapter <Call, CallViewHolder> firebaseRecyclerAdapter= new FirebaseRecyclerAdapter <Call, CallViewHolder>
+                (Call.class, R.layout.call_card, CallViewHolder.class, mRef) {
+            @Override
+            protected void populateViewHolder(CallViewHolder viewHolder, Call model, int position) {
+                viewHolder.setCaller(model.getPhNumber());
+                viewHolder.setCallType(model.getCallType());
+                viewHolder.setCallDate(model.getCallDate());
+            }
+        };
+
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+
+
+
+
+}
